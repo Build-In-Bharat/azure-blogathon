ed3aa38509d74640256131224060b1dfaa7962e8
Saurav Gupta <er.sauravgpt@gmail.com>
2024-11-12 21:40:13 +0530

    added faq and terms & condition

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 
 import Faq from "@/components/Faq";
+// import Footer from "@/components/Footer";
+import TermsAndConditions from "@/components/Terms";
 export default function Home() {
   return (
-   <div>
-<Faq/>
+   <div className="">
+    <Faq/>
+    <TermsAndConditions/>
+   
 
    </div>
   );
